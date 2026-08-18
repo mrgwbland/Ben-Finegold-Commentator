c49c51d9f9317a1ff2fcf43a08d3aa4de167586d
mrgwbland <mrgwbland@gmail.com>
2026-08-18 18:35:21 +0100

    Added moveselector fix for new lichess, more clips

--- app/scripts/Finegoldlichess.js
+++ app/scripts/Finegoldlichess.js
@@ -142,7 +142,7 @@ const observer = new MutationObserver((mutations, observerInstance) => {
   // Workaround to get $moves-tag set in
   // https://github.com/ornicar/lila/blob/master/ui/round/css/_constants.scss#L10
   // The actual element name is changed often.
-  const movesElement = document.querySelector('l4x');
+  const movesElement = document.querySelector('aPp, l4x');
 
   if (!movesElement) { return; }
 
